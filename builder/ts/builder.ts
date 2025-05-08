cc144cd5a2ed6c4e40e43b1b75142afc2cbcf123
dr1602 <david.arangelg@gmail.com>
2025-05-07 18:13:03 -0600

    feat: agrega ejercicio de builder con ts

---
+++ builder/ts/builder.ts
@@ -0,0 +1,24 @@
+class Person {
+  private name: string;
+  private lastName: string;
+  private age: number;
+  private country: string;
+  private city: string;
+  private hobbies: string[];
+
+  constructor(
+    name: string,
+    lastName: string,
+    age: number,
+    country: string,
+    city: string,
+    hobbies: string[]
+  ) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.country = country;
+    this.city = city;
+    this.hobbies = hobbies;
+  }
+}
